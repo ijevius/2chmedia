e06afe8f8b74ef5019d22c46d77d2a497ba54915
Ruslan <47752953+ijevius@users.noreply.github.com>
2020-04-23 16:05:06 +0300

    *nix fixed, supports for only new posts download
    Not compatible with old archive downloaded with this, no [subject] in folder name

--- source.py
+++ source.py
@@ -7,11 +7,38 @@ import urllib.request
 
 usercode_auth = ""
 adult_boards = ['fur', 'fet']
-DIRS_SEPARATOR = '\\' if "Windows" in platform.system() else '//'
+DIRS_SEPARATOR = '\\' if "Windows" in platform.system() else '/' 
 DVACH_MIRROR = "2ch.hk"
-DVACH_GET_THREAD_URL = f'https://{DVACH_MIRROR}/%s/res/%d.json'
 board, thr = '', 0
-ignoreExisting = ""
+DVACH_GET_THREAD_URL_2 = f"https://{DVACH_MIRROR}/makaba/mobile.fcgi?task=get_thread&board=%s&thread=%d&post=%d"
+
+def getLast(b, t):
+    #t - только номер треда
+    result = 1
+    path = f"2ch{DIRS_SEPARATOR}{b}{DIRS_SEPARATOR}{t}last"
+    if os.path.exists(path):
+        f = open(path, "r")
+        c_l = f.read()
+        result = int(c_l)
+        f.close()
+    else:
+        print(f"Этот тред еще не сохранялли")
+    print(f"Тред {t} на доске {b} start from {result}")
+    return result
+
+def writeLast(b, t, l):
+    path = f"2ch{DIRS_SEPARATOR}{b}{DIRS_SEPARATOR}{t}last"
+    summary = l
+    if os.path.exists(path):
+        f = open(path, "r")
+        c_l = f.read()
+        result = int(c_l)
+        summary += result-1
+        f.close()
+    f = open(path, 'w')
+    f.write(str(summary))
+    f.close()
+    print(f"Wrote last for {b}/{t} is {summary}")
 
 def createDirIfNotExists(dirName):
     isDir = os.path.isdir(dirName)
@@ -34,17 +61,14 @@ def makeNamePretty(str):
     #return str+"(name mod)"
     return str
 
-def fileDownload(source, dir, name, repeatingNames):
+def fileDownload(source, dir, name):
     if os.path.exists(dir + name):
         if os.path.getsize(dir + name) == 0:
             os.remove(dir + name)
         else:
-            if repeatingNames:
-                ext = "." + name.split('.')[-1]
-                name += uuid.uuid1().hex + ext
-            else:
-                print(f"{name} already exists")
-                return
+            print(f"One more file with name = {name}")
+            ext = "." + name.split('.')[-1]
+            name += uuid.uuid1().hex + ext
     try:
         opener = urllib.request.build_opener()
         opener.addheaders.append(('Cookie', f'usercode_auth={usercode_auth}; ageallow=1'))
@@ -58,41 +82,44 @@ def fileDownload(source, dir, name, repeatingNames):
 
 print('Через пробел укажите доску и номер треда.')
 board, thr = input().split(); thr = int(thr)
-ignoreExisting = input("Если в этом треде могут быть разные файлы под одним именем, введите 1.\n")
-print(ignoreExisting == "1")
-ignoreExisting = True if ignoreExisting == "1" else False
 if board in adult_boards:
     print("Чтобы смотреть некоторые доски, нужно написать любой пост и вставить сюда cookie=usercode_auth")
     usercode_auth = input()
 createDirIfNotExists("2ch")
 createDirIfNotExists(f"2ch{DIRS_SEPARATOR}{board}")
-#createDirIfNotExists(f"2ch{DIRS_SEPARATOR}{board}{DIRS_SEPARATOR}{thr}")
-DVACH_GET_THREAD_URL = DVACH_GET_THREAD_URL % (board, thr)
-print(DVACH_GET_THREAD_URL)
-r_headers = {"Cookie": ""}
+last_synced_post_for_thread = getLast(board, thr)
+DVACH_GET_THREAD_URL_2 = DVACH_GET_THREAD_URL_2 % (board, thr, last_synced_post_for_thread)
+print(DVACH_GET_THREAD_URL_2)
 
 opener = urllib.request.build_opener()
 opener.addheaders.append(('Cookie', f'usercode_auth={usercode_auth}; ageallow=1'))
-respone = "server response"
+response = "server response here"
 try:
-    response = json.loads(opener.open(DVACH_GET_THREAD_URL).read())
+    response = json.loads(opener.open(DVACH_GET_THREAD_URL_2).read())
 except:
     print("Тред или доска не существует, или usercode_auth неверный")
     sys.exit(0)
-print(response["threads"])
-thr_name = response["threads"][0]["posts"][0]["subject"]
-DIR_FOR_THREAD = f"2ch{DIRS_SEPARATOR}{board}{DIRS_SEPARATOR}{thr} -- {makeNamePretty(thr_name)}{DIRS_SEPARATOR}"
+print(response)
+if len(response) == 0:
+    print("Нет новых постов");
+    sys.exit(0)
+thr_name = response[0]["subject"]
+DIR_FOR_THREAD = f"2ch{DIRS_SEPARATOR}{board}{DIRS_SEPARATOR}{thr}{DIRS_SEPARATOR}"
+
 createDirIfNotExists(DIR_FOR_THREAD)
 withMedia = 0
-for item in response["threads"][0]["posts"]:
+total = 1
+for item in response:
     files = item["files"]
     if len(files) > 0:
         for file in files:
             origname = file["fullname"]
             origname = makeNamePretty(origname)
             url = f'https://{DVACH_MIRROR}{file["path"]}'
-            fileDownload(url, DIR_FOR_THREAD, origname, ignoreExisting)
+            fileDownload(url, DIR_FOR_THREAD, origname)
         withMedia+=1
         print(files)
+    total += 1
+writeLast(board, thr, total)
 
-print(withMedia)
+print(f"{withMedia} with media")
